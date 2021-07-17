682fde33951653993e326723eae70d1f2a2419ad
casseroles <inat0510@gmail.com>
2021-07-18 03:17:13 +0900

    single interface

--- interface/main.go
+++ interface/main.go
@@ -8,31 +8,32 @@ type Data interface {
 	PrintData()
 }
 
-// MyData is Struct.
-type MyData struct {
+// Mydata is Struct.
+type Mydata struct {
 	Name string
 	Data []int
 }
 
 // Initial is innit method.
-func (md *MyData) Initial(name string, data []int) {
+func (md *Mydata) Initial(name string, data []int) {
 	md.Name = name
 	md.Data = data
 }
 
 // Initial is innit method.
-func (md *MyData) PrintData() {
+func (md *Mydata) PrintData() {
 	fmt.Println("Name; ", md.Name)
 	fmt.Println("Data; ", md.Data)
 }
 
 // Check is method.
-func (md *MyData) Check() {
+func (md *Mydata) Check() {
+	fmt.Printf("Check! [%s]", md.Name)
 
 }
 
 func main() {
-	var ob Data = new(MyData)
+	var ob Mydata = Mydata{}
 	ob.Initial("Sachiko", []int{55, 66, 77})
-	ob.PrintData()
+	ob.Check()
 }
