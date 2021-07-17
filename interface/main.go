f4000c042d1c7a9355f02719c96dc428e1bab0ae
casseroles <inat0510@gmail.com>
2021-07-18 01:52:17 +0900

    とりあえず書き上げ

--- interface/main.go
+++ interface/main.go
@@ -1,7 +1,27 @@
 package main
 
+import "fmt"
+
 // Data is interface.
 type Data interface {
 	Initial(name string, data []int)
 	PrintData()
+}
+
+// MyData is Struct.
+type MyData struct {
+	Name string
+	Data []int
+}
+
+// Initial is innit method.
+func (md *MyData) PrintData() {
+	fmt.Println("Name; ", md.Name)
+	fmt.Println("Data; ", md.Data)
+}
+
+func main() {
+	var ob MyData = MyData{}
+	ob.Initial("Sachiko", []init{55, 66, 77})
+	ob.PrintData()
 }
